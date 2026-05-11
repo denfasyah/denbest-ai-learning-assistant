e19aa7efc7ae0c23670d53bbbc8e0bc8702e9dc6
denfasyah <denfasyah@gmail.com>
2026-05-11 14:15:54 +0700

    feat: enhance LearningPage UI with header section and icon

--- frontend/src/pages/learning/LearningPage.jsx
+++ frontend/src/pages/learning/LearningPage.jsx
@@ -3,6 +3,7 @@ import { useAuth } from "../../context/AuthContext";
 
 import useLearningDocuments from "../../hooks/useLearningDocuments";
 import useLogout from "../../hooks/useLogout";
+import { GraduationCap } from "lucide-react";
 
 import SidebarDashboard from "../../components/dashboard/layout/SidebarDashboard";
 import NavbarDashboard from "../../components/dashboard/layout/NavbarDashboard";
@@ -11,7 +12,6 @@ import EmptyDocumentLearning from "../../components/dashboard/learning/EmptyDocu
 import EmptyFavoriteLearning from "../../components/dashboard/learning/EmptyFavoriteLearning";
 import DocumentGridLearning from "../../components/dashboard/learning/DocumentGridLearning";
 import PaginationLearning from "../../components/dashboard/learning/PaginationLearning";
-
 
 const LearningPage = () => {
   const { user } = useAuth();
@@ -44,7 +44,6 @@ const LearningPage = () => {
     formatUploadTime,
   } = useLearningDocuments();
 
-
   return (
     <div className="flex min-h-screen bg-linear-to-b from-black via-[#050816] to-violet-950 text-white">
       {/* OVERLAY */}
@@ -76,6 +75,41 @@ const LearningPage = () => {
 
         {/* CONTENT */}
         <div className="px-5 py-5 lg:px-8">
+          <div
+            className="
+              mb-8 overflow-hidden
+              rounded-4xl
+              border border-white/10
+              bg-linear-to-br from-blue-500/10 via-violet-500/10 to-transparent
+              p-7
+              backdrop-blur-xl
+            "
+          >
+            {/* LEFT */}
+            <div className="flex max-w-5xl gap-8">
+              <div
+                className="
+                    mb-5 flex h-16 w-16 items-center justify-center
+                    rounded-3xl shrink-0
+                    bg-linear-to-br from-blue-500 to-violet-500
+                  "
+              >
+                <GraduationCap className="h-8 w-8 text-white" />
+              </div>
+              <div className="">
+                <h1 className="md:text-3xl text-xl font-bold tracking-[-1px]">
+                  Smart Learning Workspace
+                </h1>
+
+                <p className="mt-4 leading-relaxed text-slate-300 text-sm">
+                  Upload materi pembelajaran dan biarkan AI membantu membuat
+                  summary, quiz, flashcards, dan insight belajar.
+                </p>
+              </div>
+            </div>
+
+            {/* RIGHT */}
+          </div>
           {/* TOP ACTION */}
           <ToolbarLearning
             filter={filter}
